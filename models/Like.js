ede6b40dbbda866be79e9c628a7a46a5acb77924
Seogeurim <geulims@naver.com>
2019-11-13 19:00:14 +0900

    modify Like model

--- models/Like.js
+++ models/Like.js
@@ -1,31 +1,35 @@
 module.exports = (sequelize, DataTypes) => {
-    return sequelize.define('Like', {
-      postId: {
+  return sequelize.define(
+    "Like",
+    {
+      userId: {
         type: DataTypes.INTEGER,
         allowNull: false,
         references: {
-            model: 'Post',
-            key: 'postId',
-        },
+          model: "User",
+          key: "userId"
+        }
       },
-      userId: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-          references: {
-              model: 'User',
-              key: 'userId',
-          },
+      postId: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        references: {
+          model: "Post",
+          key: "postId"
+        }
       },
       commentId: {
-          type: DataTypes.INTEGER,
-          allowNull: true,
-          references: {
-              model: 'Comment',
-              key: 'commentId',
-          },
-      },
-    }, {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        references: {
+          model: "Comment",
+          key: "commentId"
+        }
+      }
+    },
+    {
       timestamps: true,
-      paranoid: true,
-    });
-  };
+      paranoid: true
+    }
+  );
+};
