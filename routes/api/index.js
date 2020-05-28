2e1f267093219b347daba6f906fba986b9bc967a
Seogeurim <geulims@naver.com>
2020-05-28 15:55:37 +0900

    Add Comment get/post api

--- routes/api/index.js
+++ routes/api/index.js
@@ -6,6 +6,7 @@ const authMiddleware = require("../../middleware/auth");
 const auth = require("./auth/authRouter");
 const users = require("./users/usersRouter");
 const posts = require("./posts/postsRouter");
+const comments = require("./comments/commentsRouter");
 
 router.use("/auth", auth);
 router.use("/users", users);
@@ -13,5 +14,7 @@ router.use("/users", users);
 router.use("/posts", authMiddleware);
 router.use("/posts", posts);
 
+router.use("/comments", authMiddleware);
+router.use("/comments", comments);
 
 module.exports = router;
