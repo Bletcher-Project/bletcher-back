fd6cfc2e591ac3ac471654114b2704f5c608eadf
Seogeurim <geulims@naver.com>
2020-07-19 16:41:22 +0900

    set config file #4

--- src/config/index.js
+++ src/config/index.js
@@ -1,32 +1,27 @@
-require("dotenv").config();
-const env = process.env;
+import dotenv from "dotenv";
 
-const development = {
-  username: "root",
-  password: env.MYSQL_PASSWORD,
-  database: env.MYSQL_DATABASE,
-  host: env.MYSQL_HOST,
-  dialect: "mysql",
-  operatorsAliases: "false"
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
+const envFound = dotenv.config();
+if (envFound.error) throw new Error("Couldn't find .env file");
+
+export default {
+  port: parseInt(process.env.PORT, 10),
+  logs: {
+    level: process.env.LOG_LEVEL || "silly",
+  },
+  reqAddress: process.env.REQ_ADDRESS,
+  database: {
+    username: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    host: process.env.MYSQL_HOST,
+    database: process.env.MYSQL_DATABASE,
+    dialect: "mysql",
+    operatorsAliases: "false",
+    logging: "false",
+  },
+  jwtKey: process.env.JWT_KEY,
+  api: {
+    prefix: "/api",
+  },
 };
-
-const production = {
-  username: "root",
-  password: env.MYSQL_PASSWORD,
-  database: env.MYSQL_DATABASE,
-  host: env.MYSQL_HOST,
-  dialect: "mysql",
-  operatorsAliases: "false",
-  logging: "false"
-};
-
-const test = {
-  username: "root",
-  password: env.MYSQL_PASSWORD,
-  database: env.MYSQL_DATABASE_TEST,
-  host: env.MYSQL_HOST,
-  dialect: "mysql",
-  operatorsAliases: "false"
-};
-
-module.exports = { development, production, test };
