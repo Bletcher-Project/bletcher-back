bcba2fd519b1220c8d515420f2b6b46b665fc52e
Seogeurim <geulims@naver.com>
2020-08-26 22:03:33 +0900

    update env example file #27

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,11 @@
 import * as dotenv from 'dotenv';
-const cloudinary = require('cloudinary').v2;
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 const envFound = dotenv.config();
 if (envFound.error) throw new Error("Couldn't find .env file");
+
+const cloudinary = require('cloudinary').v2;
 
 export default {
   port: parseInt(process.env.PORT as string, 10),
@@ -12,9 +13,9 @@ export default {
     level: process.env.LOG_LEVEL || 'silly',
   },
   cloudinary: () => {
-    cloudinary.config({ 
-      cloud_name: process.env.CLOUD_NAME, 
-      api_key: process.env.API_KEY, 
+    cloudinary.config({
+      cloud_name: process.env.CLOUD_NAME,
+      api_key: process.env.API_KEY,
       api_secret: process.env.API_SECRET,
     });
   },
