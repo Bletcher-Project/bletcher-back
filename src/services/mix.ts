6d2304200e9964144344ee566cedb9f2d3f9f54b
donggyu-kim <kimss7334@naver.com>
2020-11-14 16:38:23 +0900

    fix postmix() method

--- src/services/mix.ts
+++ src/services/mix.ts
@@ -1,7 +1,6 @@
 import Mix from '../models/mix';
 import { IMixInfo, IPostdetail } from '../interfaces/post';
 import { createPost, getPostByPostId } from './post';
-import { getUsernameById } from './user';
 
 export const checkMixExists = async (params: IMixInfo): Promise<boolean> => {
   const mix: Mix | null = await Mix.findOne({
@@ -11,33 +10,26 @@ export const checkMixExists = async (params: IMixInfo): Promise<boolean> => {
 };
 
 export const postMix = async (params: IMixInfo): Promise<void> => {
-  const sampleOriginPost = await getPostByPostId(params.origin_post_id);
-  const sampleSubPost = await getPostByPostId(params.sub_post_id);
-
-  const originUserId = sampleOriginPost?.getDataValue('user_id');
-  const subUserId = sampleSubPost?.getDataValue('user_id');
-
-  const originUserName = await getUsernameById(originUserId as number);
-  const subUserName = await getUsernameById(subUserId as number);
-
-  const mixTitle = `${originUserName!.nickname} X ${subUserName!.nickname}`;
-
-  const newpostinfo = {
-    title: mixTitle,
-    description: null,
-    is_public: true,
-    user_id: originUserId,
-    category_id: 1,
-    image_id: sampleOriginPost?.getDataValue(
-      'image_id',
-    ) /* image_id will come from getImageFromAI */,
-  };
-  const mixedPost = await createPost(newpostinfo as IPostdetail);
-  await Mix.create({
-    origin_post_id: params.origin_post_id,
-    sub_post_id: params.sub_post_id,
-    post_id: mixedPost?.getDataValue('id'),
-  });
+  const sampleOriginPost: any = await getPostByPostId(params.origin_post_id);
+  const sampleSubPost: any = await getPostByPostId(params.sub_post_id);
+  if (sampleOriginPost && sampleSubPost) {
+    const mixTitle = `${sampleOriginPost['User.nickname']} X ${sampleSubPost['User.nickname']}`;
+    const newpostinfo = {
+      title: mixTitle,
+      description: null,
+      is_public: true,
+      user_id: sampleOriginPost['User.id'],
+      category_id: 1,
+      image_id: sampleOriginPost['Image.id'],
+      /* image_id will come from getImageFromAI */
+    };
+    const mixedPost = await createPost(newpostinfo as IPostdetail);
+    await Mix.create({
+      origin_post_id: params.origin_post_id,
+      sub_post_id: params.sub_post_id,
+      post_id: mixedPost?.getDataValue('id'),
+    });
+  }
 };
 
 export const getOriginMixInfo = async (id: number): Promise<Mix[] | null> => {
